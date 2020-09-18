0c051718fcbb7b762ca3cebf59da0f3e0e220181
kdbnk23 <68780982+kdbnk23@users.noreply.github.com>
2020-09-18 15:48:14 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,14 @@
+class Box {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.Visiblity = 255;
+    }
+    
+  };
+  display()
+    push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop();
+  
